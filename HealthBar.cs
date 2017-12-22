2a0112cf84b810a176c12b08568af6bd33339aa3
waffel183 <koensanderse2000@gmail.com>
2017-12-22 12:34:47 +0100

    UI bar scripts

---
+++ HealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    public GameObject player;
+    float barSpeed;
+    public Image bar;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        barSpeed = player.GetComponent<PlayerHealth>().ReturnHealth();
+        bar.fillAmount = barSpeed / 100;
+    }
+}
